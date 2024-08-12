4cb35e74225bfb24ee0f0a212cf77727b11a24be
Sameer Abid <Sameerabid56@gmail.com>
2024-08-13 02:36:13 +0500

    added plugin

--- routes/users.js
+++ routes/users.js
@@ -27,6 +27,6 @@ userSchema.methods.comparePassword = function(candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
-
+userSchema.plugin(plm);
 
 module.exports = router;
